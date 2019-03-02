4e52355494e8931749297683adea9bb48801998d
timothylampung <timothy.lampung@canang.com.my>
2019-03-02 14:42:58 +0800

    Completable

--- app/src/main/java/com/timothy/todoapp/service/ReminderService.kt
+++ app/src/main/java/com/timothy/todoapp/service/ReminderService.kt
@@ -2,14 +2,22 @@ package com.timothy.todoapp.service
 
 import com.google.gson.Gson
 import com.timothy.todoapp.room.AppDatabase
+import com.timothy.todoapp.room.reminder.Reminder
 import com.timothy.todoapp.room.reminder.ReminderDao
+import io.reactivex.Completable
+
 
 class ReminderService(private val json: Gson,
                       private val database: AppDatabase) {
 
     val reminderDao: ReminderDao = this.database.reminderDao()
 
-    //TODO save reminder
+    fun save(reminder: Reminder): Completable? {
+       return Completable.fromAction {
+            //TODO
+        }
+    }
+
     //TODO get reminder
     //TODO delete reminder
     //TODO update reminder
--- app/src/main/java/com/timothy/todoapp/view/main/MainActivityViewModel.kt
+++ app/src/main/java/com/timothy/todoapp/view/main/MainActivityViewModel.kt
@@ -5,4 +5,14 @@ import com.timothy.todoapp.service.ReminderService
 
 class MainActivityViewModel(private val reminderService: ReminderService) : ViewModel(){
 
+
+    //TODO save reminder
+    //TODO get reminder
+    //TODO delete reminder
+    //TODO update reminder
+
+
+    companion object {
+        private const val TAG = "RmndrVm"
+    }
 }
